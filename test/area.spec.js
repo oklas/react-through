986e989a5bcfe350c654b6b8c2881f0887629bc3
Serguei Okladnikov <oklaspec@gmail.com>
2018-03-29 22:16:19 +0300

    test removing from area

--- test/area.spec.js
+++ test/area.spec.js
@@ -29,14 +29,16 @@ const TestContainer = throughArea('test_area')(TestComponent)
 const TestAgent = throughAgentFactory(new String('test_area'), 'id' )
 const TestAgentB = throughAgentFactory('test_area', props => 'b')
 
-const TestApp = ({value, show_i, show_u}) => (
+const TestApp = ({value, show_b, show_i, show_u}) => (
   <ThroughProvider>
     <main>
       <header>
         <TestContainer />
       </header>
       <article>
-        <TestAgentB value={1 + value} />
+        { show_b &&
+          <TestAgentB value={1 + value} />
+        }
         { show_i &&
           <TestAgent id='i' value={2 + value} />
         }
@@ -50,23 +52,31 @@ const TestApp = ({value, show_i, show_u}) => (
 
 describe('test with multiple agents', function() {
   it("transfer and update the data", function() {
-    const wrapper = mount(<TestApp value={10}/>)
+    const wrapper = mount(<TestApp show_b value={10}/>)
 
     expect(wrapper.find('header').find('b')).to.have.length(1)
     expect(wrapper.find('header').find('i')).to.have.length(0)
     expect(wrapper.find('header').find('u')).to.have.length(0)
     expect(wrapper.find('header').find('b').at(0).props().children).to.equal(11)
 
-    wrapper.setProps({show_i: true})
+    wrapper.setProps({show_b: false, show_i: true})
+    wrapper.update()
+
+    expect(wrapper.find('header').find('b')).to.have.length(0)
+    expect(wrapper.find('header').find('i')).to.have.length(1)
+    expect(wrapper.find('header').find('u')).to.have.length(0)
+    expect(wrapper.find('header').find('i').at(0).props().children).to.equal(12)
+
+    wrapper.setProps({show_b: true, show_i: false, show_u: true})
     wrapper.update()
 
     expect(wrapper.find('header').find('b')).to.have.length(1)
-    expect(wrapper.find('header').find('i')).to.have.length(1)
-    expect(wrapper.find('header').find('u')).to.have.length(0)
+    expect(wrapper.find('header').find('i')).to.have.length(0)
+    expect(wrapper.find('header').find('u')).to.have.length(1)
     expect(wrapper.find('header').find('b').at(0).props().children).to.equal(11)
-    expect(wrapper.find('header').find('i').at(0).props().children).to.equal(12)
+    expect(wrapper.find('header').find('u').at(0).props().children).to.equal(13)
 
-    wrapper.setProps({show_i: true, show_u: true})
+    wrapper.setProps({show_b: true, show_i: true, show_u: true})
     wrapper.update()
 
     expect(wrapper.find('header').find('b')).to.have.length(1)
