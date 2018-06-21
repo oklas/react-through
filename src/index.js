b6c4144224b11eed248949892fb3516f1de6d19d
jkeruzec <jkeruzec@systemplus.fr>
2018-06-21 10:26:29 +0200

    Add babel polyfill support for ie11 (find polyfill)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Children } from 'react'
 import PropTypes from 'prop-types'
+import 'babel-polyfill'
 
 export ThroughProvider from './ThroughProvider'
 export throughContainer from './throughContainer'
